800973f83745d1b6cd284d4122e08a52ba647b27
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in.com>
2026-07-04 13:48:05 +0530

    Make article pages public

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,21 @@
 import Link from 'next/link';
 import UserAvatar from '../components/UserAvatar';
 
-
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center py-6 px-10 border-b border-gray-800 bg-[#0a0a0a] text-white">
       <Link href="/" className="text-2xl font-extrabold tracking-tighter">
         NEXUS
       </Link>
-      <div className="flex items-center gap-4">
-        <Link href="/" className="hover:text-black">Home</Link>
-        <Link href="/article" className="hover:text-black">Articles</Link>
-        <Link href="/auth/signin" className="bg-black text-white px-5 py-2 rounded-full hover:bg-gray-800 transition">
+      
+      {/* Container hidden on mobile, flex on desktop */}
+      <div className="hidden md:flex items-center gap-6">
+        <Link href="/" className="hover:text-gray-400 transition">Home</Link>
+        <Link href="/article" className="hover:text-gray-400 transition">Articles</Link>
+        <Link href="/auth/signin" className="bg-white text-black px-5 py-2 rounded-full hover:bg-gray-300 transition">
           Sign In
         </Link>
         <UserAvatar />
-        
       </div>
     </nav>
   );
